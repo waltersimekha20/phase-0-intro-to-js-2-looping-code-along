7012619763d504791b644ce00fe37f252730f011
waltersimekha <waltersimekha@gmail.com>
2023-03-21 08:46:06 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const names =["Guadalupe", "Ollie", "Aki"]
+
+
+function writeCards(names,event = 'graduation'){
+    let message= [];
+    for(let i= 0;i < names.length;i++){
+        message.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        
+    }
+    return message;
+};
+function countDown(num){
+   while(num>=0){
+    
+    console.log(num--);
+   }
+
+}
+countDown(10);
